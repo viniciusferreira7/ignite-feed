0497a0e6957a4dab9e5ba01cf96abcf38a80ddbc
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-06-23 21:25:54 -0300

    utilizando o css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,11 @@
-import { useState } from 'react'
+import React from 'react'
+import { Header } from './components/Header'
+import './styles.css'
 
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export  function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <>
+      <Header/>
+    </>
   )
 }
-
-export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+export function Header() {
+  return (
+    <strong className={styles.header}>Ignite Feed</strong>
+  )
+}
