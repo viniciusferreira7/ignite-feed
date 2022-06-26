ef58ccf62d65a2d2f2a73f1f113ae53ee789abf1
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-06-26 11:09:50 -0300

    Aplicando css no <Sidebar />

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,17 @@ import React from 'react';
 import { Header } from './components/Header';
 import { Post } from './components/Post';
 
-import styled from './App.module.css'
+import styles from './App.module.css'
 
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export  function App() {
   return (
     <>
       <Header/>
-      <div className={styled.wrapper}>
-        <aside>
-          Aside
-        </aside>
+      <div className={styles.wrapper}>
+       <Sidebar/>
         <main>
           <Post author='Vinicius De Aquino' content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu arcu justo.'/>
         </main>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
-import styled from './Sidebar.module.css'
+import styles from './Sidebar.module.css'
 
 export function Sidebar() {
   return (
-    <aside className={styled.sidebar}>Sidebar</aside>
+    <aside className={styles.sidebar}>
+      <img className={styles.cover} src='https://images.unsplash.com/photo-1579403124614-197f69d8187b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50
+      '/>
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/83302144?v=4" alt="" />
+
+        <strong>Vinicius Aquino</strong>
+        <p>Front End Develop</p>
+      </div>
+      <footer>
+        <a href="#">
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
   )
 }
