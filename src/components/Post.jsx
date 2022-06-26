127bdea203d15d76498c8c8ccfaf7c15399be590
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-06-26 15:47:59 -0300

    Falta apenas o criação da seção de comentarios do <Post />

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -17,13 +17,17 @@ export function Post() {
       <time title="26 de Junho ás 13:30" dateTime="2022-06-26">Publicado há 1h</time>
     </header>
     <div className={styles.content}>
-      <p>Fala galeraa </p>👋
+      <p>Fala galeraa👋</p>
 
      <p> Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
 
-     <p> 👉 <a href=''>jane.design/doctorcare</a></p>
+     <p><a href=''>jane.design/doctorcare</a></p>
 
-     <p><a href=''>#novoprojeto #nlw #rocketseat</a></p>
+     <p>
+      <a href=''>#novoprojeto</a>{' '}
+       <a href=''>#nlw</a>{' '}
+       <a href=''>#rocketseat</a>{' '}
+      </p>
     </div>
    </article>
   )
