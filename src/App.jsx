755b9f592e177e05fa9ce977ed4d8a53a5f9a14f
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-06-28 18:58:29 -0300

    Fazendo uma iteração no <App /> com o <Post />

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,50 @@ import styles from './App.module.css'
 
 import './global.css';
 import { Sidebar } from './components/Sidebar';
+import { Key } from 'phosphor-react';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl:'https://avatars.githubusercontent.com/u/83302144?v=4',
+      name:'Vinicius De Aquino',
+      role:'Front End Developer'
+    },
+    content:[
+      {
+        type:'paragraph', content: 'Fala galeraa👋'
+      },
+      {
+        type:'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'
+      },
+      {
+        type:'link', content :'jane.design/doctorcare'
+      }
+    ],
+    publishedAt: new Date('2022-06-28 18:27'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl:'https://github.com/maykbrito.png',
+      name:'Mayk Brito',
+      role:'Educador @Rocketseat'
+    },
+    content:[
+      {
+        type:'paragraph', content: 'Fala galeraa👋'
+      },
+      {
+        type:'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'
+      },
+      {
+        type:'link', content :'jane.design/doctorcare'
+      }
+    ],
+    publishedAt: new Date('2022-06-08 18:27'),
+  },
+]
 
 export  function App() {
   return (
@@ -14,7 +58,16 @@ export  function App() {
       <div className={styles.wrapper}>
        <Sidebar/>
         <main>
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post 
+                Key={post.id}
+                author={post.author} 
+                content={post.content} 
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </>
