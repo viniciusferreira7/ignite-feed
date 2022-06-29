2df89473705f7f6c761bc40bd7721cdf3b60f31a
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-06-28 21:22:34 -0300

    Formatando as datas e inserindo os dados no <Post />

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,35 +1,44 @@
 import React from 'react'
+
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 
 import styles from './Post.module.css'
 
-export function Post() {
+export function Post({ author, publishedAt, content }) {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'ás' HH:mm'h'",{
+    locale:ptBR
+  })
+
+  const publishedDateRelativeNow = formatDistanceToNow(publishedAt, {
+    locale:ptBR,
+    addSuffix:true
+  })
+
   return (
    <article className={styles.post}>
     <header>
       <div className={styles.author}>
-        <Avatar src="https://avatars.githubusercontent.com/u/83302144?v=4"/>
+        <Avatar src={author.avatarUrl}/>
         <div className={styles.authorInfo}>
-          <strong>Vinicius De Aquino</strong>
-          <span>Front End Developer</span>
+          <strong>{author.name}</strong>
+          <span>{author.role}</span>
         </div>
       </div>
 
-      <time title="26 de Junho ás 13:30" dateTime="2022-06-26">Publicado há 1h</time>
+      <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeNow}</time>
     </header>
     <div className={styles.content}>
-      <p>Fala galeraa👋</p>
-
-     <p> Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-     <p><a href=''>jane.design/doctorcare</a></p>
-
-     <p>
-      <a href=''>#novoprojeto</a>{' '}
-       <a href=''>#nlw</a>{' '}
-       <a href=''>#rocketseat</a>{' '}
-      </p>
+      {content.map(line => {
+        if (line.type == 'paragraph') {
+          return <p>{line.content}</p>
+        } else if (line.type == 'link') {
+          return <p><a href="#">{line.content}</a></p>
+        }
+      })}
     </div>
 
     <form className={styles.commentForm}>
